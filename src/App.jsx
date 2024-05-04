464ac62425cb0bf6e21f1a34d5b5c7e2420eeacd
B1smarQ <bgh050021@gmail.com>
2024-05-04 22:59:09 +0400

    123

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import Header from './components/Header.jsx'
+import {why} from './why.js'
+import Footer from './components/Footer.jsx'
+function WhyKustikPidor(props){
+  return(
+    <ul>
+      <li>
+        <h2>
+            {props.title}
+      </h2>
+      <p>{props.name}</p>
+      </li>
+    </ul>
+  )
+}
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <WhyKustikPidor title = {why[0].title} name = {why[0].name}/>
+      <WhyKustikPidor title = {why[1].title} name = {why[1].name}/>
+      <WhyKustikPidor title = {why[2].title} name = {why[2].name}/>
+      <WhyKustikPidor title = {why[3].title} name = {why[3].name}/>
+      <WhyKustikPidor title = {why[4].title} name = {why[4].name}/> 
+    </div>
+  )
+}
+
+export default App
